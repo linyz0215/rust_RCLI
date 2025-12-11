f451856ad9494197375f56c380deffa9431fd24e
linyz0215 <1902829444@qq.com>
2025-12-11 13:09:27 +0800

    saving work

---
+++ src/opts.rs
@@ -0,0 +1,39 @@
+use std::path::Path;
+
+use clap::Parser;
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about = "lalalalallalalal", long_about = None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "Show CSVS, or Convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    pub input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if Path::new(filename).exists() {
+        Ok(filename.to_string())
+    } else {
+        Err("Input file does not exist.")
+    }
+}
+//生命周期 box::leak,'static 
